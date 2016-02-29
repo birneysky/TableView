05194e7af8e61fcfe756bed0f942f31a2e64c522
birneysky <birneysky@163.com>
2016-02-29 16:29:13 +0800

    implement LCSectionInfo

--- Models/LCSectionInfo.m
+++ Models/LCSectionInfo.m
@@ -10,4 +10,53 @@
 
 @implementation LCSectionInfo
 
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        _rowHeights = [[NSMutableArray alloc] init];
+    }
+    return self;
+}
+
+- (NSUInteger)countOfRowHeights
+{
+    return [self.rowHeights count];
+}
+
+- (id)objectInRowHeightsAtIndex:(NSUInteger)index
+{
+    return self.rowHeights[index];
+}
+
+- (void)insertObject:(id)object inRowHeightsAtIndex:(NSUInteger)index
+{
+    [self.rowHeights insertObject:object atIndex:index];
+}
+
+- (void)insertRowHeights:(NSArray *)array atIndexes:(NSIndexSet *)indexes
+{
+    [self.rowHeights insertObjects:array atIndexes:indexes];
+}
+
+- (void)removeObjectFromRowHeightsAtIndex:(NSUInteger)index
+{
+    [self.rowHeights removeObjectAtIndex:index];
+}
+
+- (void)removeRowHeightsAtIndexes:(NSIndexSet *)indexes
+{
+    [self.rowHeights removeObjectsAtIndexes:indexes];
+}
+
+- (void)replaceObjectInRowHeightsAtIndex:(NSUInteger)index withObject:(id)object
+{
+    self.rowHeights[index] = object;
+}
+
+- (void)replaceRowHeightsAtIndexes:(NSIndexSet *)indexes withRowHeights:(NSArray *)array
+{
+    [self.rowHeights replaceObjectsAtIndexes:indexes withObjects:array];
+}
+
 @end
