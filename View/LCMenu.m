b4b5098f57a87841f8dde7fe587a2ffd5abad4fb
yingwei0831 <birneysky@163.com>
2016-03-17 22:40:39 +0800

    modify

--- View/LCMenu.m
+++ View/LCMenu.m
@@ -14,12 +14,15 @@
 {
     UIBezierPath* path = [UIBezierPath bezierPath];
     [path moveToPoint:CGPointMake(20, 0)];
-    [path addLineToPoint:CGPointMake(10, 10)];
-    [path addLineToPoint:CGPointMake(30, 10)];
+    [path addLineToPoint:CGPointMake(12, 10)];
+    [path addLineToPoint:CGPointMake(28, 10)];
     [[UIColor redColor] setFill];
     [path fill];
     
-
+    CGRect rect1 = CGRectMake(0, 10, self.bounds.size.width, self.bounds.size.height - 10);
+    UIBezierPath* rectPath = [UIBezierPath bezierPathWithRoundedRect:rect1 cornerRadius:5.0f];
+    [[UIColor redColor] setFill];
+    [rectPath fill];
 }
 
 @end
